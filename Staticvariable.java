7a6c6ad16d4e1b8c269b5f12a04980e70631d8fc
Nachiket Kulkarni <nachiketkulkarni19@gmail.com>
2026-01-29 08:57:11 +0530

    Added OOPS programs

---
+++ Staticvariable.java
@@ -0,0 +1,14 @@
+class A {
+    int a;
+}
+
+public class Staticvariable {
+    static int n;
+
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.a = 10;
+        n = obj.a;
+        System.out.println("Value of n: " + n);
+    }
+}
