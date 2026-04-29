e5a22cc56d59dee37968989959036c4ca031a0c9
Nachiket Kulkarni <nachiketkulkarni19@gmail.com>
2026-04-29 12:24:23 +0530

    Added Successfully

---
+++ Final.java
@@ -0,0 +1,17 @@
+class A {
+    final int var1=10;
+    final void some(){
+        System.out.println("Hello Sirr");
+    }
+}
+final class B{
+    int var2=20;
+}
+class C extends B{
+
+}
+public class Final{
+    public static void main(String[] args){
+        var1.a=a.var1
+    }
+}
